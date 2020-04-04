c28e9f11de253e1b4610997963bc71e5b6abe11d
febrifahmi <fahmi_fafa@yahoo.com>
2020-04-05 06:05:04 +0700

    Adding user page, avatar, last seen, etc.

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from flask import request
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
+from datetime import datetime
 
 @app.route('/')
 
@@ -15,7 +16,7 @@ from app.forms import RegistrationForm
 def index():
     #return "Hello, World!"
     posts = [{'author':{'username':current_user.username}, 'body': 'first post.'}]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home', user=current_user, posts=posts)
 
 # @app.route('/login', methods=['GET', 'POST'])
 # def login():
@@ -72,3 +73,19 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
