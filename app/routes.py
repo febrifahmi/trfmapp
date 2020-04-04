c15d4eee0051aa2272cd18efff1ed42bd3bc371b
febrifahmi <fahmi_fafa@yahoo.com>
2020-04-04 09:36:45 +0700

    Progress until registration funzction

--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,74 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from flask import request
+from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
 
 @app.route('/')
 
 @app.route('/index')
+@login_required
 def index():
     #return "Hello, World!"
-    user = {'username': 'Febri'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    posts = [{'author':{'username':current_user.username}, 'body': 'first post.'}]
+    return render_template('index.html', title='Home', posts=posts)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+# 	form = LoginForm()
+# 	if form.validate_on_submit():
+# 		flash('Login requested for user {}, remember_me={}'.format(
+#         	form.username.data, form.remember_me.data))
+# 		return redirect(url_for('index'))
+# 	return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(
-        	form.username.data, form.remember_me.data))
-		return redirect(url_for('index'))
-	return render_template('login.html', title='Sign In', form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+        # return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/about')
 def about():
-	user = {'username': 'Febri'}
 	posts = [
 		{
 			'author': {'username': 'Admin'},
 			'body': 'This is an about page!'
 		}
 	]
-	return render_template('about.html', title='About', user=user, posts=posts)
+	return render_template('about.html', title='About', posts=posts)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
--- trfmapp.py
+++ trfmapp.py
@@ -1,2 +1,7 @@
 # Main code of trfmapp
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
